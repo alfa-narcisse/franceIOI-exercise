5d8fbcea931709d545535ffd03d11706c0b2140a
Alfa Narcisse <alfanambinintsoa@gmail.com>
2025-08-23 16:39:37 +0100

    resolution de l'exercice 5

--- level3.py
+++ level3.py
@@ -1,2 +1,16 @@
 # Exercice 5: déchet polluant 
 
+N,M = list(map(int,input().split()))
+stck = list(map(int,input().split()))
+chargement = ""
+def findMax(s):
+    i = 0
+    for j in range(len(s)):
+        if s[j]>s[i]:
+            i = j
+    return s.pop(i)
+
+for k in range(M):
+    chargement += " "+str(findMax(stck))
+
+print(chargement[1:])
