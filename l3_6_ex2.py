2128f868a378fc8100e728e119b83555308b539a
Alfa Narcisse <alfanambinintsoa@gmail.com>
2025-09-12 22:19:11 +0100

    Just making some change

--- l3_6_ex2.py
+++ l3_6_ex2.py
@@ -1,3 +1,11 @@
 # Gestion du stock (utilisant l'algorithme dichotomique)
 N,M = list(map(int,input().split()))
 Stck = list(map(int,input().split()))
+stckToInsert = list(map(int,input().split()))
+
+def findPosition(stck, item):
+    if len(stck) == 0:
+        return 0
+    else:
+        if 
+        
